08aa60c7a397d5ab8eed6af3c7a57c06e45287ef
florentinojink <florentinojink@163.com>
2024-04-18 15:17:43 +0800

    Add mini-redis simple client and server

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream){
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("Got: {:?}", frame);
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
