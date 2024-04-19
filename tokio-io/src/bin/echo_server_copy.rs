84bf6f32f6e53a92426c3e370df770206a70b515
florentinojink <florentinojink@163.com>
2024-04-19 11:33:43 +0800

    toml add bin path, add echo server_copy

---
+++ tokio-io/src/bin/echo_server_copy.rs
@@ -0,0 +1,14 @@
+use tokio::io;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            todo!()
+        });
+    }
+}
