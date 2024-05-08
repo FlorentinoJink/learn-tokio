799a27a1ef3aaacb12272ed69586b0d0f5b86c03
florentinojink <florentinojink@163.com>
2024-05-08 15:23:38 +0800

    mini tokio

--- mini-tokio/src/main.rs
+++ mini-tokio/src/main.rs
@@ -1,3 +1,68 @@
+use futures::task;
+use std::collections::VecDeque;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if Instant::now() >= self.when {
+            println!("hello world");
+            Poll::Ready("done")
+        } else {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut mini_tokio = MiniTokio::new();
+
+    mini_tokio.spawn(async {
+        let when = Instant::now() + Duration::from_millis(3000);
+        let future = Delay { when };
+
+        let out = future.await;
+        assert_eq!(out, "done");
+    });
+
+    mini_tokio.run();
+}
+type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+
+struct MiniTokio {
+    task: VecDeque<Task>,
+}
+
+impl MiniTokio {
+    fn new() -> MiniTokio {
+        MiniTokio {
+            task: VecDeque::new(),
+        }
+    }
+
+    fn spawn<F>(&mut self, future: F)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        self.task.push_back(Box::pin(future));
+    }
+
+    fn run(&mut self) {
+        let waker = task::noop_waker();
+        let mut cx = Context::from_waker(&waker);
+
+        while let Some(mut task) = self.task.pop_front() {
+            if task.as_mut().poll(&mut cx).is_pending() {
+                self.task.push_back(task);
+            }
+        }
+    }
 }
